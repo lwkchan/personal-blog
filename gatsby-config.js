4c725baabed45c5a1708623c79b7a4154745aa3c
lwkchan <laurachan205@gmail.com>
2018-08-22 19:03:32 +0100

    api: set up link to Contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
+const contentfulConfig = require('./contentful-config')
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    {
+      resolve: `gatsby-source-contentful`,
+      options: contentfulConfig,
+    },
+  ],
 }
