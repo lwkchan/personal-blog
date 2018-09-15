3c48fa955ba8c490eb0712cfe9186d7595475231
lwkchan <laurachan205@gmail.com>
2018-09-15 11:12:05 +0100

    styles: modify header.js to use css modules

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,14 @@
 import Link from 'gatsby-link'
 import React from 'react'
+import styles from './header.module.css'
 
 const Header = ({ headerText }) =>
 (
-  <div
-    style={{
-      background: 'orange',
-      marginBottom: '1.50rem'
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem'
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'black'
-          }}
-        >
-          {headerText}
+  <div className={styles.background}>
+    <div className={styles.content}>
+      <h1 className={styles.heading}>
+        <Link to="/">
+          headerText
         </Link>
       </h1>
     </div>
